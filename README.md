c26f2c79173e2808b21fc0e3dca579c9be8d62e1
Melve <evife2@gmail.com>
2019-04-16 00:58:43 +0200

    Update, completion of 1st tasks part.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# TSig
-Eopsy - Project Two
+Project Two for EOPSY classes WUT.
+
+Overwiev
+
+Program creates children processes, number of which is defined as NUM_CHILD. Delay between consecutive creation request is defined as PARENT_SLEEP.
+If creation of child process is unsuccessful, terminate already existing processes(SIGTERM signal) and exit program with code 1.
+Newly created processes are set to sleep for time defined as CHILD_SLEEP. After \'nap\' children are terminated with exit code 0. Parent process awaits(synchronise) termination of created children and saves its id and exit status.
+Then summary is displayed(id and exit status of each created process during run-time).
+
+Author Michal Tchorzewski
--- tsig.c
+++ tsig.c
@@ -3,40 +3,98 @@
 #include <unistd.h>
 #include <signal.h>
 
-pid_t create_process(){
+#define NUM_CHILD 7
+#define CHILD_SLEEP 10
+#define PARENT_SLEEP 1
 
-	pid_t new_process=fork();
+struct process{
+        int id;
+        int exitStatus;
+};
 
-	if(new_process<0)
-		printf("*(fun)\r\tSomething went wrong\n");
-	else if(new_process==0){
-		printf("*(fun)\r\tSleep %i - Wait for 10\'\'...\n", new_process);
-		sleep(10);
-		printf("*(fun)\r\tChild is awake\n");
-		exit(0);
-	}else if(new_process>0){
-		printf("*(fun)\r\tChild ID returned to Parent Process %i - %i\n", new_process, getpid());
+struct process table[NUM_CHILD]; // GLOBAL: Create array capable of storing all of the child id.
+
+void terminate(){
+	for(int terminated=0; terminated<NUM_CHILD; terminated++){
+		if(table[terminated].id>0)
+			printf("*(fun terminate)\tKilled child: %d - %d\n", table[terminated].id, kill(table[terminated].id, SIGTERM));
+		else
+			break;
+	}
+}
+
+pid_t create_process(){ // Description of functionality below.
+	// fun: Creates child process, whenever called.
+	// fun: Terminates program if child process creation is unsuccessful with approperiate message.
+	// fun: Sets new process to sleep for time defined in CHILD_SLEEP, after sleep completion exit child process with code 0. 
+	//      Informs user about current state of child process.
+	// fun: Displays child and parent process id.
+	// fun: Returns child process(creation successful) id or -1(creation unsuccessful) 
+	
+	pid_t new_process=fork(); // Create new process and store its status.
+
+	if(new_process<0){ // Fork returns negative value if allocation of new process was unsuccessful.
+		fprintf(stderr, "*(fun create_process)\tSomething went wrong\n"); // Display Error Message. 
+	}else if(new_process==0){ // 0 - is returned to child process.
+		printf("*(fun create_process)\tChild Process created successfully.\n");
+		printf("*(fun creatr_process)\tSleep Child - Sleep for %d sec...\n", CHILD_SLEEP); 
+		// Display Creation Message.
+		sleep(CHILD_SLEEP); // Set to sleep.
+		printf("*(fun create_process)\tChild is awake\n"); // Display Awake Message.
+		exit(0); // Exit child process with exit code 0.
+	}else if(new_process>0){ // Newly created process id is returned to parent process.
+		printf("*(fun create_process)\tChild ID returned to Parent Process %i, Parent Process %i\n", new_process, getpid()); 
+		// Display Child and Parent process id.
 	}
 
-	return new_process;
+	return new_process; // Child process id is returned or -1
+}
+
+void children_factory(){
+	// fun: creates defined children number.
+	// fun: saves child id into table.
+	for(int created=0; created<NUM_CHILD; created++){ // Create number of children processes defined in NUM_CHILD
+		if ((table[created].id=(int)create_process())<0){ // Store Child Process inside array. Cast pid_t to int.
+                	terminate(); // Terminate already created processes.
+			exit(1); // Exit parent process with code 1.
+		}
+			
+		sleep(PARENT_SLEEP); // sleep parent process for time defined in PARENT_SLEEP, before calling for new process creation.
+        }
+}
+
+void synchronise(){
+	// fun: awaits children processes to be terminated.
+	// fun: saves exit status of each child process.
+	int status; // variable holds exit status of process.
+
+        for(int terminated=0; terminated<NUM_CHILD; terminated++){ // Wait for all children processes to terminate.
+                pid_t terminatedChildId=waitpid(table[terminated].id, &status, 0); // Wait for child process with correspond id
+                // to one stored in table.
+                table[terminated].exitStatus=WEXITSTATUS(status); // Set&Save exit code of child process. 
+                // 0 - no option, parent has to wait for child termination.
+        }
+}
+
+void summary(){ 
+	// fun: displays id and exit status of child process, stored in elemnt of array.
+        for(int element=0; element<NUM_CHILD; element++){ // Program Summary 
+                printf("\tChild %d, terminated with status: %d\n", table[element].id, table[element].exitStatus);
+        }
 }
 
 int main(void){
 
-	printf("Main:\r\tTSig process: %i\n\n", getpid());
-	int processIdTable[2];
-	
-	processIdTable[0]=(int)create_process();
-	sleep(2);
-	processIdTable[1]=(int)create_process();
-	sleep(2);
+	printf("Main:\r\tTSig process: %i\n\n", getpid()); // Display Parent Process Id
 
-	printf("\n---------------------------------------------------------------------------------------------\n");
-	printf("1.\r\tReturned value is %i\n\r\tChild process %i was terminated.\n", processIdTable[0], wait(NULL));
-	printf("\n---------------------------------------------------------------------------------------------\n");
-	printf("2.\r\tReturned value is %i\n\r\tChild process %i was terminated.\n", processIdTable[1], wait(NULL));
-	printf("\n---------------------------------------------------------------------------------------------\n");
+	children_factory(); // Create NUM_CHILD processes.
 
+	synchronise(); // Wait for processes termination then carry on execution.
+
+	if(wait(NULL)==-1) // Check whethere there is no more children processes.
+		printf("\nThere is no more Children Processes.\n\nSummary:\n");
 	
+	summary(); // Display id and exit status of children processes.
+
 	return 0;
 }
